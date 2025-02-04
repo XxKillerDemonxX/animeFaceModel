16a2af15d73a5e607c5956a39ab684027d9381ec
XxKillerDemonxX <adamtan443@gmail.com>
2025-02-03 17:02:18 -0800

    updated some comments

--- model.py
+++ model.py
@@ -34,14 +34,14 @@ lr = 0.0002         #learning rate
 beta1 = 0.5         #beta1 hyperparameter
 ngpu = 1            #number of gpus available
 
-
+#dataset
 dataset = dset.ImageFolder(root = dataroot,
                            transform=transforms.Compose([transforms.Resize(image_size),
                                                             transforms.CenterCrop(image_size),
                                                             transforms.ToTensor(),
                                                             transforms.Normalize((0.5, 0.5,0.5), (0.5, 0.5, 0.5)),
                                                         ]))
-
+#loading the dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
 
 #what device to run on (gpu or cpu)
@@ -50,15 +50,12 @@ print(torch.cuda.is_available())
 #print(torch.__version__)
 
 
-
+#function to add random noise for an input (supposedly it helps the model train)
 def add_noise(images, noise_std=0.1):
     return images + noise_std * torch.randn_like(images)
 
 
 #convolutional layer
-#out_channels can also represent how many filters (kernels) are in the layer
-#if 64x64 rgb image (3 channels), dimensions would be 64,64,3. in_channels would be 3
-#filter_size is always smaller than image dimensions
 class ConvolutionalLayer(nn.Module):
     def __init__(self, in_channels, out_channels, filter_size, stride=1, padding=0, dilation = 1, bias=True, padding_mode='zeros', device=device):
         super(ConvolutionalLayer, self).__init__()
@@ -71,47 +68,43 @@ class ConvolutionalLayer(nn.Module):
         self.device = device
         #initialize weights and biases
         self.weight = torch.randn(out_channels, in_channels, filter_size, filter_size, device = self.device) * ((2/in_channels) * 0.2)
-        self.weight.requires_grad_()
+        self.weight.requires_grad_() # requires grad so that gradients are calculated
         self.kernel = (filter_size, filter_size)
         if bias==True:
             self.bias = torch.randn(in_channels, filter_size, filter_size, device = self.device, requires_grad = True)
         else:
             self.bias = torch.zeros_like(in_channels, filter_size, filter_size, device = self.device, requires_grad = False)
     def __call__(self, x):
-
+        #find the new image height and width after its reduced
         image_height = ((x.shape[2] + (self.padding * 2) - self.filter_size) // self.stride) + 1
         image_width = ((x.shape[3] + (self.padding * 2) - self.filter_size) // self.stride) + 1
-
-        #pad all edges of the matrix with a 0
+        #pad all edges of the matrix with a 0 so that kernel can capture the edges
         x = torch.nn.functional.pad(x, (self.padding, self.padding, self.padding, self.padding), mode = 'constant', value = 0)
         #unfold ex. 4x3x9x9 (3x3 kernel) -> 4x27x81
         unfold = nn.Unfold(
             kernel_size=(self.filter_size, self.filter_size), 
             stride = self.stride
         )
-
         x = unfold(x)
-        #permute so it flattens the right elements or something, view to make sure its in_channels*filter_size*filter_size, out_channels
-
+        #permute to bring to in_channel, filter_size, filter_size, out_channels
         self.weight = self.weight.permute(1, 2, 3, 0)
-
+        #view to make sure its in_channels*filter_size*filter_size, out_channels
         self.weight = self.weight.view(-1, self.out_channels)
-
-        #transpose the number of patches and elements in patch so that it can match the shape of the weights for matrix mult.
-
-        x = x.permute(0, 2, 1)@self.weight#+ self.bias
-        
-        #reshape so number of patches can go back to image sizes
-        #print(x.shape)
+        #permute x so that its batch_size, patches, patch_size
+        #matrix multiply by weight, patch_size dimension should match in_channels*filter_size*filter_size
+        x = x.permute(0, 2, 1)@self.weight # -> batch_size, patches, out_channel
+        #permute it to batch_size, out_channels, patches
         x = x.permute(0, 2, 1)
+        #reshape it to batch_size, out_channels, image_height, image_width
         x = x.reshape(batch_size, self.out_channels, image_height, image_width)
-
-        #make sure weight is back to correct dimensions
+        #make sure weight is back to correct dimensions, or we could leave default weight shape as in_channels*filter_size*filter_size, out_channels, but this might make it easier to understand
         self.weight = self.weight.view(self.in_channels, self.filter_size, self.filter_size, self.out_channels)
         self.weight = self.weight.permute(3, 0, 1, 2)
         self.out = x
+        #return the output
         return self.out
     def parameters(self):
+        #return all the parameters involved (bias is not currently used, but return it anyways)
         return [self.weight, self.bias]
 
 class ConvolutionalTransposeLayer(nn.Module):
@@ -132,6 +125,7 @@ class ConvolutionalTransposeLayer(nn.Module):
         else:
             self.bias = torch.zeros(out_channels, device = self.device, requires_grad = False)
     def get_output_size(self, input_size):
+        #return the output size of an input based on stride and filter size
         return (
             (input_size - 1) * self.stride
             - 2 * self.padding + self.dilation * (self.filter_size - 1)
@@ -139,22 +133,25 @@ class ConvolutionalTransposeLayer(nn.Module):
             + 1
         )
     def __call__(self, x):
+        #view weights as in_channels, out_channels*filter_size*filter_size
         self.weight = self.weight.view(self.in_channels, -1)
-        #make a size function to get size after being passed through a transpose convolutional layer
+        #get the output size, we only need one because our image is a square, would need two different if not a square though
         output_size = (self.get_output_size(x.shape[2]), self.get_output_size(x.shape[3]))
-
+        #view x as batch_size, in_channels, image_height*image_width
+        #permute x to batch_size, image_height*image_width, in_channels
         x = x.view(batch_size, self.in_channels, -1).permute(0, 2, 1)
+        #matrix multiply by weight
+        #output is batch_size, image_height*image_width, out_channels*filter_size*filter_size
         output = x@self.weight
+        #permute to batch_size, out_channels*filter_size*filter_size, image_height*image_width
         output = output.permute(0, 2, 1)
+        #fold - folds the output to an image of desired dimensions
+        #sums up all the matrixes ontop of each other correctly
         output = torch.nn.functional.fold(output, output_size, stride = self.stride, kernel_size=self.filter_size, padding=self.padding)
-        #output += self.bias
         self.out = output
-
         #return weight to orginal dimensions
         self.weight = self.weight.reshape(self.in_channels, self.out_channels, self.filter_size, self.filter_size)
-        #print(self.out.shape)
         return self.out
-
     def parameters(self):    
         return [self.weight, self.bias]
 
@@ -170,9 +167,11 @@ class BatchNorm(nn.Module):
         xmean = x.mean(dim=(0, 2, 3), keepdim = True)
         #variance of all elements, doesn't mix between channels
         xvar = x.var(dim=(0, 2, 3), keepdim = True)
+        #batchnorm equation
+        #add e-5 incase xvar is 0
         y = (x - xmean) / torch.sqrt(xvar + 0.00001)
+        #multiply by weight and add some bias
         self.out = self.weight * y + self.bias
-
         return self.out
     def parameters(self):
         return [self.weight, self.bias]
@@ -204,7 +203,6 @@ class Generator(nn.Module):
             #x = 1, 64, 32, 32
             ConvolutionalTransposeLayer(ngf, nc, filter_size=4, stride=2, padding=1, bias=False, device=device),      BatchNorm(nc),    Tanh()
             #x = 1, 3, 64, 64  
-
         ]
     def __call__(self, x):
         for layer in self.layers:
@@ -228,9 +226,6 @@ class Discriminator(nn.Module):
             ConvolutionalLayer(ndf*2, ndf*4, 4, 2, 1, device=device), BatchNorm(ndf*4), LeakyRelu(),
             ConvolutionalLayer(ndf*4, ndf*8, 4, 2, 1, device=device), BatchNorm(ndf*8), LeakyRelu(),
             ConvolutionalLayer(ndf*8, 1, 4, 1, 0, device=device)
-
-
-
         ]
     def __call__(self, x):
         for layer in self.layers:
@@ -266,7 +261,7 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0001, betas=(0.5, 0.999))
     optimizerG = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
     #training loop
-    for i in range(5):
+    for i in range(10):
         for i, data in enumerate(dataloader, 0):
             images, label = data
             if images.size(0) < batch_size:
@@ -329,15 +324,21 @@ if __name__ == '__main__':
     plt.title('Generator and Discriminator Losses over Iterations')
     plt.legend()
 
-# Display the plot
+    # Display the plot
     plt.show()
     print("done")
 
     matrixI = torch.randn(128, nz, 1, 1, device=device)
     fake = generator(matrixI)
-    image_tensor = fake[0]
-    image_np = image_tensor.permute(1, 2, 0).detach().cpu().numpy()
-    image_np = (image_np + 1) / 2
-    plt.imshow(image_np)
-    plt.axis('off')  # Hide axes
+    # image_tensor = fake[0]
+    # image_np = image_tensor.permute(1, 2, 0).detach().cpu().numpy()
+    # image_np = (image_np + 1) / 2
+    # plt.imshow(image_np)
+    # plt.axis('off')  # Hide axes
+    # plt.show()
+    # Create a grid for displaying images
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Generated Images")
+    plt.imshow(np.transpose(vutils.make_grid(fake.to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
     plt.show()
